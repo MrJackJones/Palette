48e0537e8268f24bfd2059f1b3bf2fb12b690bf9
back2 <back2@local.ru>
2022-03-06 11:50:38 +0300

    Initial Commit

---
+++ Palette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Palette
+//
+//  Created by Ivan on 06.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
